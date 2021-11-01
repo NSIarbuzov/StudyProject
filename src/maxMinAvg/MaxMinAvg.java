5cc6973ffbb6c86457df6a32d97a3da7e4a4ac84
vozubra <vozubra@inbox.ru>
2021-11-01 11:05:56 +0300

    First Commit

---
+++ src/maxMinAvg/MaxMinAvg.java
@@ -0,0 +1,27 @@
+package maxMinAvg;
+
+public class MaxMinAvg {
+    public static void main(String[] args) {
+        double[] array = new double[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+
+        }
+
+        double max = array[0];
+        double min = array[0];
+        double avg = 0;
+        for (double v : array) {
+            if (max < v)
+                max = v;
+            if (min > v)
+                min = v;
+            avg += v / array.length;
+        }
+
+        System.out.println("max = " + String.format("%.2f", max));
+        System.out.println("min = " + String.format("%.2f", min));
+        System.out.println("avg = " + String.format("%.2f", avg));
+
+    }
+}
