cc7a5bb51ef6f30ebdba93051c3f5040391f3021
vozubra <vozubra@inbox.ru>
2021-11-12 00:37:50 +0300

    Initial commit

--- src/naturalNumber/NaturalNumber.java
+++ src/naturalNumber/NaturalNumber.java
@@ -2,7 +2,7 @@ package naturalNumber;
 
 public class NaturalNumber {
     public static void main(String[] args) {
-        int a = 5, b = 10;
+        int a = 2, b = 10;
         for (int i = a; i <= b; i++) {
             System.out.print(i + " ");
         }
